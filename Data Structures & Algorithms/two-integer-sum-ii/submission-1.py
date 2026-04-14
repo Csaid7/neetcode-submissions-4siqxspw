6f65a49a4b6caff91828e7b0712a4817e633f9ef
Csaid7 <166649141+Csaid7@users.noreply.github.com>
2026-04-13 21:32:53 -0400

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # use two pointers
+        #r,l ex: [0,1,2,3,4] target = 7
+        #r = 4 l = 0
+        # look for sum  ==  4 + 0 < 7
+        # since sum < target move l pointer up 
+        # else move right pointer down 
+        r , l = len(numbers) - 1, 0
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum  == target:
+                return [ l + 1  , r + 1]
+            if sum < target:
+                l += 1 
+            else:
+                r -= 1
+        return []
